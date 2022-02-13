76b9ed25259c4513ec4284918f6ea35a84d7b89e
anastasia1756 <anastasiia7kolomiiets@gmail.com>
2022-02-13 13:29:12 +0200

    Updates

---
+++ precache-manifest.236bf05b97041ec32b63759198e95d7a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16ba6db9fd7dcb7e03a5680dc382aedb",
+    "url": "/stripe-submenus-project/index.html"
+  },
+  {
+    "revision": "724356bc1b8a6e567e31",
+    "url": "/stripe-submenus-project/static/css/main.58ae8225.chunk.css"
+  },
+  {
+    "revision": "9d9ce4078b442a71cc60",
+    "url": "/stripe-submenus-project/static/js/2.e1e77988.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/stripe-submenus-project/static/js/2.e1e77988.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "724356bc1b8a6e567e31",
+    "url": "/stripe-submenus-project/static/js/main.d0f067cf.chunk.js"
+  },
+  {
+    "revision": "e64b6238773155d5eaf8",
+    "url": "/stripe-submenus-project/static/js/runtime-main.b8339d2d.js"
+  },
+  {
+    "revision": "8596ab4a635cf6b6ea535b15bdc0dc67",
+    "url": "/stripe-submenus-project/static/media/hero.8596ab4a.svg"
+  },
+  {
+    "revision": "1090473dd728fcc17033df70118fa668",
+    "url": "/stripe-submenus-project/static/media/logo.1090473d.svg"
+  },
+  {
+    "revision": "58d7e3d689c0cb8eb365b86046ecdce8",
+    "url": "/stripe-submenus-project/static/media/phone.58d7e3d6.svg"
+  }
+]);
